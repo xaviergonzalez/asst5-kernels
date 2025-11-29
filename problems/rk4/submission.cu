1b4fcbf1412ebe1626503c085d018b4fed57f6aa
Xavier Gonzalez <xavier18@sh04-05n05.int>
2025-11-29 12:04:41 -0800

    getting caught up

--- problems/rk4/submission.cu
+++ problems/rk4/submission.cu
@@ -5,6 +5,9 @@
 // Create your kernel functions here
 // Example: __global__ void kernel(...) { ... }
 // 
+__global__ void fused_kernel(){
+
+}
 
 // Host function to launch kernel
 torch::Tensor custom_kernel(
@@ -27,6 +30,26 @@ torch::Tensor custom_kernel(
         throw std::runtime_error("All dimensions must be >= 9 for radius-4 stencil.");
     }
 
+    // 3D 8th-order 2nd-derivative Laplacian coefficients
+    float c0 = -205.0 / 72.0
+    float c1 =   8.0  /  5.0
+    float c2 =  -1.0  /  5.0
+    float c3 =   8.0  / 315.0
+    float c4 =  -1.0  / 560.0
+
+    // CFL stability (same constant, but now for RK4)
+    float c = 0.05
+
+    float inv_hx2 = 1.0 / (hx * hx)
+    float inv_hy2 = 1.0 / (hy * hy)
+    float inv_hz2 = 1.0 / (hz * hz)
+
+    float S  = inv_hx2 + inv_hy2 + inv_hz2
+    float dt = c / (alpha * S)
+
+    // Radius of stencil
+    int r = 4
+
     // Allocate output tensor (or reuse u0 for in-place)
     torch::Tensor result = u0.clone();  // TODO: Modify as needed
     
@@ -35,7 +58,9 @@ torch::Tensor custom_kernel(
     ////
     // Launch your kernel here
     ////
-    
+    dim3 gridDim = 
+    dim3 blockDim
+    fused_kernel<<>>()
 
 
 
