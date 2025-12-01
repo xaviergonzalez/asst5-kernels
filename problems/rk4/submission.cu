9945e119b587a62843269dcc508f1ded1e0f6087
Xavier Gonzalez <xavier18@stanford.edu>
2025-11-30 20:30:21 -0800

    first draft of rk3

--- problems/rk4/submission.cu
+++ problems/rk4/submission.cu
@@ -13,25 +13,18 @@ struct GlobalConstants
     float dt;
     int Nz, Ny, Nx;
     int r;
+    float alpha;
     
 };
 __constant__ GlobalConstants cuConstParams;
 
-__global__ void compute_laplacian(
+// helper function for computing k1
+__device__ __inline__ void
+get_laplacian_at_point(
     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> lap,
-)
+    int tx, int ty, int tz)
 {
-    int tx = threadIdx.x + blockIdx.x * blockDim.x;
-    int ty = threadIdx.y + blockIdx.y * blockDim.y;
-    int tz = threadIdx.z + blockIdx.z * blockDim.z;
-
-    // Boundary checks
-    if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
-        return;
-
-    // Compute 8th-order Laplacian here using stencil coefficients
-    // and store result in lap
     float u_xx = (cuConstParams.c0 * u[tz, ty, tx] + 
     cuConstParams.c1 * (u[tz, ty, tx-1] + u[tz, ty, tx+1]) + 
     cuConstParams.c2 * (u[tz, ty, tx-2] + u[tz, ty, tx+2]) + 
@@ -49,13 +42,126 @@ __global__ void compute_laplacian(
                   cuConstParams.c3 * (u[tz-3, ty, tx] + u[tz+3, ty, tx]) +
                   cuConstParams.c4 * (u[tz-4, ty, tx] + u[tz+4, ty, tx])) *
                  cuConstParams.inv_hz2;
-    lap[tz, ty, tx] = u_xx + u_yy + u_zz;
+    lap[tz, ty, tx] = cuConstParams.alpha * (u_xx + u_yy + u_zz);
 }
 
-__global__ void rk4(
-    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_new,
-    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_old,
-    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> lap)
+// helper function for computing k2, k3, k4
+__device__ __inline__ void
+get_laplacian_two_points(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> lap,
+    int tx, int ty, int tz, float second_weight)
+{
+    float u_xx = (cuConstParams.c0 * u[tz, ty, tx] +
+                  cuConstParams.c1 * (u[tz, ty, tx - 1] + u[tz, ty, tx + 1]) +
+                  cuConstParams.c2 * (u[tz, ty, tx - 2] + u[tz, ty, tx + 2]) +
+                  cuConstParams.c3 * (u[tz, ty, tx - 3] + u[tz, ty, tx + 3]) +
+                  cuConstParams.c4 * (u[tz, ty, tx - 4] + u[tz, ty, tx + 4])) *
+                 cuConstParams.inv_hx2;
+    float u_yy = (cuConstParams.c0 * u[tz, ty, tx] +
+                  cuConstParams.c1 * (u[tz, ty - 1, tx] + u[tz, ty + 1, tx]) +
+                  cuConstParams.c2 * (u[tz, ty - 2, tx] + u[tz, ty + 2, tx]) +
+                  cuConstParams.c3 * (u[tz, ty - 3, tx] + u[tz, ty + 3, tx]) +
+                  cuConstParams.c4 * (u[tz, ty - 4, tx] + u[tz, ty + 4, tx])) *
+                 cuConstParams.inv_hy2;
+    float u_zz = (cuConstParams.c0 * u[tz, ty, tx] +
+                  cuConstParams.c1 * (u[tz - 1, ty, tx] + u[tz + 1, ty, tx]) +
+                  cuConstParams.c2 * (u[tz - 2, ty, tx] + u[tz + 2, ty, tx]) +
+                  cuConstParams.c3 * (u[tz - 3, ty, tx] + u[tz + 3, ty, tx]) +
+                  cuConstParams.c4 * (u[tz - 4, ty, tx] + u[tz + 4, ty, tx])) *
+                 cuConstParams.inv_hz2;
+    // now repeat same computation but for keys
+    float k_xx = (cuConstParams.c0 * k[tz, ty, tx] +
+                  cuConstParams.c1 * (k[tz, ty, tx - 1] + k[tz, ty, tx + 1]) +
+                  cuConstParams.c2 * (k[tz, ty, tx - 2] + k[tz, ty, tx + 2]) +
+                  cuConstParams.c3 * (k[tz, ty, tx - 3] + k[tz, ty, tx + 3]) +
+                  cuConstParams.c4 * (k[tz, ty, tx - 4] + k[tz, ty, tx + 4])) *
+                 cuConstParams.inv_hx2;
+    float k_yy = (cuConstParams.c0 * k[tz, ty, tx] +
+                  cuConstParams.c1 * (k[tz, ty - 1, tx] + k[tz, ty + 1, tx]) +
+                  cuConstParams.c2 * (k[tz, ty - 2, tx] + k[tz, ty + 2, tx]) +
+                  cuConstParams.c3 * (k[tz, ty - 3, tx] + k[tz, ty + 3, tx]) +
+                  cuConstParams.c4 * (k[tz, ty - 4, tx] + k[tz, ty + 4, tx])) *
+                 cuConstParams.inv_hy2;
+    float k_zz = (cuConstParams.c0 * k[tz, ty, tx] +
+                  cuConstParams.c1 * (k[tz - 1, ty, tx] + k[tz + 1, ty, tx]) +
+                  cuConstParams.c2 * (k[tz - 2, ty, tx] + k[tz + 2, ty, tx]) +
+                  cuConstParams.c3 * (k[tz - 3, ty, tx] + k[tz + 3, ty, tx]) +
+                  cuConstParams.c4 * (k[tz - 4, ty, tx] + k[tz + 4, ty, tx])) *
+                 cuConstParams.inv_hz2;
+    lap[tz, ty, tx] = cuConstParams.alpha * (u_xx + u_yy + u_zz + second_weight * (k_xx + k_yy + k_zz));
+}
+
+// __global__ void compute_laplacian(
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> lap,
+// )
+// {
+//     int tx = threadIdx.x + blockIdx.x * blockDim.x;
+//     int ty = threadIdx.y + blockIdx.y * blockDim.y;
+//     int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+//     // Boundary checks
+//     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+//         return;
+
+//     // Compute 8th-order Laplacian here using stencil coefficients
+//     // and store result in lap
+//     float u_xx = (cuConstParams.c0 * u[tz, ty, tx] + 
+//     cuConstParams.c1 * (u[tz, ty, tx-1] + u[tz, ty, tx+1]) + 
+//     cuConstParams.c2 * (u[tz, ty, tx-2] + u[tz, ty, tx+2]) + 
+//     cuConstParams.c3 * (u[tz, ty, tx-3] + u[tz, ty, tx+3]) + 
+//     cuConstParams.c4 * (u[tz, ty, tx-4] + u[tz, ty, tx+4])) * cuConstParams.inv_hx2;
+//     float u_yy = (cuConstParams.c0 * u[tz, ty, tx] +
+//                   cuConstParams.c1 * (u[tz, ty - 1, tx] + u[tz, ty + 1, tx]) +
+//                   cuConstParams.c2 * (u[tz, ty - 2, tx] + u[tz, ty + 2, tx]) +
+//                   cuConstParams.c3 * (u[tz, ty - 3, tx] + u[tz, ty + 3, tx]) +
+//                   cuConstParams.c4 * (u[tz, ty - 4, tx] + u[tz, ty + 4, tx])) *
+//                  cuConstParams.inv_hy2;
+//     float u_zz = (cuConstParams.c0 * u[tz, ty, tx] +
+//                   cuConstParams.c1 * (u[tz-1, ty, tx] + u[tz+1, ty, tx]) +
+//                   cuConstParams.c2 * (u[tz-2, ty, tx] + u[tz+2, ty, tx]) +
+//                   cuConstParams.c3 * (u[tz-3, ty, tx] + u[tz+3, ty, tx]) +
+//                   cuConstParams.c4 * (u[tz-4, ty, tx] + u[tz+4, ty, tx])) *
+//                  cuConstParams.inv_hz2;
+//     lap[tz, ty, tx] = u_xx + u_yy + u_zz;
+// }
+
+// __global__ void rk4(
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_new,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_old,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> lap)
+// {
+//     int tx = threadIdx.x + blockIdx.x * blockDim.x;
+//     int ty = threadIdx.y + blockIdx.y * blockDim.y;
+//     int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+//     // Boundary checks
+//     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+//         return;
+//     // never implemented bc I realized we could fuse
+// }
+
+// __global__ void fused_kernel(
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_new,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_old)
+// {
+//     int tx = threadIdx.x + blockIdx.x * blockDim.x;
+//     int ty = threadIdx.y + blockIdx.y * blockDim.y;
+//     int tz = threadIdx.z + blockIdx.z * blockDim.z;
+//     // Boundary checks (only update interior points)
+//     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+//         return;
+//     // Laplacian computation
+    
+//     float lap = u_xx + u_yy + u_zz;
+//     // RK4 update
+// }
+
+__global__ void get_k1(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k1)
 {
     int tx = threadIdx.x + blockIdx.x * blockDim.x;
     int ty = threadIdx.y + blockIdx.y * blockDim.y;
@@ -64,24 +170,133 @@ __global__ void rk4(
     // Boundary checks
     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
         return;
-    // never implemented bc I realized we could fuse
+
+    get_laplacian_at_point(u, k1, tx, ty, tz);
+    
 }
 
-__global__ void fused_kernel(
-    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_new,
-    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u_old)
+__global__ void get_k2(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k1,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k2,
+)
 {
     int tx = threadIdx.x + blockIdx.x * blockDim.x;
     int ty = threadIdx.y + blockIdx.y * blockDim.y;
     int tz = threadIdx.z + blockIdx.z * blockDim.z;
-    // Boundary checks (only update interior points)
+
+    // Boundary checks
     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
         return;
-    // Laplacian computation
-    
-    float lap = u_xx + u_yy + u_zz;
-    // RK4 update
+
+    get_laplacian_two_points(u, k1, k2, tx, ty, tz, 0.5 * cuConstParams.dt);
 }
+
+__global__ void get_k3(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k2,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k3)
+{
+    int tx = threadIdx.x + blockIdx.x * blockDim.x;
+    int ty = threadIdx.y + blockIdx.y * blockDim.y;
+    int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+    // Boundary checks
+    if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+        return;
+    get_laplacian_two_points(u, k2, k3, tx, ty, tz, 0.5 * cuConstParams.dt);
+}
+
+__global__ void get_k4(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k3,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k4)
+{
+    int tx = threadIdx.x + blockIdx.x * blockDim.x;
+    int ty = threadIdx.y + blockIdx.y * blockDim.y;
+    int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+    // Boundary checks
+    if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+        return;
+    get_laplacian_two_points(u, k3, k4, tx, ty, tz, cuConstParams.dt);
+}
+
+__global__ void combine_rk4_step(
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k1,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k2,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k3,
+    torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k4
+)
+{
+    int tx = threadIdx.x + blockIdx.x * blockDim.x;
+    int ty = threadIdx.y + blockIdx.y * blockDim.y;
+    int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+    // Boundary checks
+    if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+        return;
+
+    u[tz, ty, tx] = u[tz, ty, tx] + (cuConstParams.dt / 6.0) * (k1[tz, ty, tx] + 2.0 * k2[tz, ty, tx] + 2.0 * k3[tz, ty, tx] + k4[tz, ty, tx]);
+}               
+
+// __global__ void rk4_step(
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> u,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k1,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k2,
+//     torch::PackedTensorAccessor32<float, 3, torch::RestrictPtrTraits> k3,
+// )
+// {
+//     int tx = threadIdx.x + blockIdx.x * blockDim.x;
+//     int ty = threadIdx.y + blockIdx.y * blockDim.y;
+//     int tz = threadIdx.z + blockIdx.z * blockDim.z;
+
+//     // Boundary checks
+//     if (tx >= Nx - cuConstParams.r || ty >= Ny - cuConstParams.r || tz >= Nz - cuConstParams.r || tx < cuConstParams.r || ty < cuConstParams.r || tz < cuConstParams.r)
+//         return;
+
+//     float k4;
+//     float u_xx = (cuConstParams.c0 * u[tz, ty, tx] +
+//                   cuConstParams.c1 * (u[tz, ty, tx - 1] + u[tz, ty, tx + 1]) +
+//                   cuConstParams.c2 * (u[tz, ty, tx - 2] + u[tz, ty, tx + 2]) +
+//                   cuConstParams.c3 * (u[tz, ty, tx - 3] + u[tz, ty, tx + 3]) +
+//                   cuConstParams.c4 * (u[tz, ty, tx - 4] + u[tz, ty, tx + 4])) *
+//                  cuConstParams.inv_hx2;
+//     float u_yy = (cuConstParams.c0 * u[tz, ty, tx] +
+//                   cuConstParams.c1 * (u[tz, ty - 1, tx] + u[tz, ty + 1, tx]) +
+//                   cuConstParams.c2 * (u[tz, ty - 2, tx] + u[tz, ty + 2, tx]) +
+//                   cuConstParams.c3 * (u[tz, ty - 3, tx] + u[tz, ty + 3, tx]) +
+//                   cuConstParams.c4 * (u[tz, ty - 4, tx] + u[tz, ty + 4, tx])) *
+//                  cuConstParams.inv_hy2;
+//     float u_zz = (cuConstParams.c0 * u[tz, ty, tx] +
+//                   cuConstParams.c1 * (u[tz - 1, ty, tx] + u[tz + 1, ty, tx]) +
+//                   cuConstParams.c2 * (u[tz - 2, ty, tx] + u[tz + 2, ty, tx]) +
+//                   cuConstParams.c3 * (u[tz - 3, ty, tx] + u[tz + 3, ty, tx]) +
+//                   cuConstParams.c4 * (u[tz - 4, ty, tx] + u[tz + 4, ty, tx])) *
+//                  cuConstParams.inv_hz2;
+//     // now repeat same computation but for keys
+//     float k_xx = (cuConstParams.c0 * k[tz, ty, tx] +
+//                   cuConstParams.c1 * (k[tz, ty, tx - 1] + k[tz, ty, tx + 1]) +
+//                   cuConstParams.c2 * (k[tz, ty, tx - 2] + k[tz, ty, tx + 2]) +
+//                   cuConstParams.c3 * (k[tz, ty, tx - 3] + k[tz, ty, tx + 3]) +
+//                   cuConstParams.c4 * (k[tz, ty, tx - 4] + k[tz, ty, tx + 4])) *
+//                  cuConstParams.inv_hx2;
+//     float k_yy = (cuConstParams.c0 * k[tz, ty, tx] +
+//                   cuConstParams.c1 * (k[tz, ty - 1, tx] + k[tz, ty + 1, tx]) +
+//                   cuConstParams.c2 * (k[tz, ty - 2, tx] + k[tz, ty + 2, tx]) +
+//                   cuConstParams.c3 * (k[tz, ty - 3, tx] + k[tz, ty + 3, tx]) +
+//                   cuConstParams.c4 * (k[tz, ty - 4, tx] + k[tz, ty + 4, tx])) *
+//                  cuConstParams.inv_hy2;
+//     float k_zz = (cuConstParams.c0 * k[tz, ty, tx] +
+//                   cuConstParams.c1 * (k[tz - 1, ty, tx] + k[tz + 1, ty, tx]) +
+//                   cuConstParams.c2 * (k[tz - 2, ty, tx] + k[tz + 2, ty, tx]) +
+//                   cuConstParams.c3 * (k[tz - 3, ty, tx] + k[tz + 3, ty, tx]) +
+//                   cuConstParams.c4 * (k[tz - 4, ty, tx] + k[tz + 4, ty, tx])) *
+//                  cuConstParams.inv_hz2;
+
+//        u[tz, ty, tx] = u[tz, ty, tx] + (cuConstParams.dt / 6.0) * (k1[tz, ty, tx] + 2.0 * k2[tz, ty, tx] + 2.0 * k3[tz, ty, tx] + k4);
+// }
 
 // Host function to launch kernel
 torch::Tensor custom_kernel(
@@ -138,18 +353,27 @@ torch::Tensor custom_kernel(
     params.Ny = Ny;
     params.Nx = Nx;
     params.r = r;
+    params.alpha = alpha;
 
     cudaMemcpyToSymbol(cuConstParams, &params, sizeof(GlobalConstants));
 
 
     // Allocate output tensor (or reuse u0 for in-place)
     u0 = u0.contiguous();
-    torch::Tensor lap = torch::zeros_like(u0);
-    lap = lap.contiguous();
-    torch::Tensor result = u0.clone();  // TODO: Modify as needed
+    torch::Tensor k1 = torch::empty_like(u0);
+    k1 = k1.contiguous();
+    torch::Tensor k2 = torch::empty_like(u0);
+    k2 = k2.contiguous();
+    torch::Tensor k3 = torch::empty_like(u0);
+    k3 = k3.contiguous();
+    torch::Tensor k4 = torch::empty_like(u0);
+    k4 = k4.contiguous();
     auto u_acc = u0.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
-    auto res_acc = result.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
-    auto lap_acc = lap.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
+    auto k1_acc = k1.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
+    auto k2_acc = k2.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
+    auto k3_acc = k3.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
+    auto k4_acc = k4.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
+    // auto lap_acc = lap.packed_accessor32<float, 3, torch::RestrictPtrTraits>();
     // const float *d_u0 = u0.data_ptr<float>();
     // float *d_result = result.data_ptr<float>();
 
@@ -163,14 +387,41 @@ torch::Tensor custom_kernel(
     // Launch your kernel here
     ////
     for _ in range(n_steps):
-        compute_laplacian<<<numBlocks, threadsPerBlock>>>(
+        get_k1<<<numBlocks, threadsPerBlock>>>(
             u_acc,
-            lap_acc,
+            k1_acc,
         );
-        rk4<<<numBlocks, threadsPerBlock>>>(
-            res_acc,
+        get_k2<<<numBlocks, threadsPerBlock>>>(
             u_acc,
+            k1_acc,
+            k2_acc,
         );
+        get_k3<<<numBlocks, threadsPerBlock>>>(
+            u_acc,
+            k2_acc,
+            k3_acc,
+        );
+        get_k4<<<numBlocks, threadsPerBlock>>>(
+            u_acc,
+            k3_acc,
+            k4_acc,
+        );
+        combine_rk4_step<<<numBlocks, threadsPerBlock>>>(
+            u_acc,
+            k1_acc,
+            k2_acc,
+            k3_acc,
+            k4_acc
+        );
+        // compute_laplacian<<<numBlocks, threadsPerBlock>>>(
+        //     u_acc,
+        //     lap_acc,
+        // );
+        // rk4<<<numBlocks, threadsPerBlock>>>(
+        //     res_acc,
+        //     u_acc,
+        // );
+
 
     // Check for errors
     cudaError_t err = cudaGetLastError();
@@ -181,6 +432,7 @@ torch::Tensor custom_kernel(
     // Synchronize to ensure kernel completion
     cudaDeviceSynchronize();
     
-    return result;
+    return u0;
+    // return result;
 }
 
